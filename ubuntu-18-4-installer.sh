96109cff63f7952195d418d8d7233b65c5b433e5
Frank Schultze <frank@dovecor.com>
2019-07-19 09:16:42 +0200

    first commit

---
+++ ubuntu-18-4-installer.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+  sudo apt update
+  sudo apt upgrade
+  sudo apt install ffmpeg
+  mkdir dab
+  cd dab/
+  sudo apt-get -y install build-essential git wget  sox alsa-tools alsa-utils/
+  automake libtool mpg123  libasound2 libasound2-dev  libjack-jackd2-dev jackd2/
+  ncdu vim ntp links cpufrequtils  libfftw3-dev  libcurl4-openssl-dev/
+  libmagickwand-dev  libvlc-dev vlc-data  libfaad2 libfaad-dev  python-mako python-requests
+  sudo apt-get -y install libzmq5-dev libzmq5
+  edit soursec.list uncomment - deb http://archive.ubuntu.com/ubuntu bionic universe
+  sudo apt-get -y build-dep uhd
+  sudo vi /etc/apt/sources.list
+  sudo apt update
+  git clone http://github.com/EttusResearch/uhd.git
+  pushd uhd
+  git checkout release_003_010_003_000
+  mkdir build
+  cd build
+  cmake ../host
+  make
+  sudo make install
+  popd
+  git clone https://github.com/mpbraendli/mmbtools-aux.git
+  git clone https://github.com/Opendigitalradio/fdk-aac.git
+  pushd fdk-aac
+  ./bootstrap
+  ./configure
+  make
+  sudo make install
+  popd
+  sudo ldconfig
+  git clone https://github.com/Opendigitalradio/ODR-PadEnc.git
+  pushd ODR-PadEnc
+  ./bootstrap
+  ./configure
+  make
+  sudo make install
+  popd
+  git clone https://github.com/Opendigitalradio/ODR-AudioEnc.git
+  pushd ODR-AudioEnc
+  ./bootstrap
+  ./configure --enable-jack --enable-vlc --enable-alsa
+  make
+  sudo make install
+  popd
+  sudo apt update
